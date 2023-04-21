62ef7a517c3c78985dbe01bafde8288cfa9fe2ec
Shiv <shivasare101@gmail.com>
2023-04-21 12:58:23 +0530

    DSA add file

---
+++ DSA/dsa1_timeSpaceComplexity.java
@@ -0,0 +1,11 @@
+package DSA;
+public class dsa1_timeSpaceComplexity{
+    public static void main(String[] args) {
+        int n=8;
+        int sum=10;
+        for (int i = 0; i < args.length; i++) {
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
