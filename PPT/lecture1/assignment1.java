b9be45db8d05f7ab5e9802acd6322b07cc716bbb
Shiv <shivasare101@gmail.com>
2023-06-21 13:08:16 +0530

    add file,file name=PPT

---
+++ PPT/lecture1/assignment1.java
@@ -0,0 +1,7 @@
+package PPT.lecture1;
+
+public class assignment1 {
+    public static void main(String[] args) {
+        System.out.println("ebnjrngbjne");
+    }
+}
---
+++ PPT/lecture1/practice1.java
@@ -0,0 +1,7 @@
+//package PPT.lecture1;
+
+public class practice1 {
+    public static void main(String[] args) {
+        System.out.println("menenuuhuhuhuhuhuhugbjnjnja");
+    }
+}
